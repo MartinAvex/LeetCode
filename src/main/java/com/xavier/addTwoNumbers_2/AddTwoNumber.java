7396be8dc4a771c48993bf74e64c79d1202fe215
吴峥嵘 <xaviercharles@163.com>
2024-07-16 09:17:36 +0800

    [MartinAvex]: LeetCode

---
+++ src/main/java/com/xavier/addTwoNumbers_2/AddTwoNumber.java
@@ -0,0 +1,86 @@
+package com.xavier.addTwoNumbers_2;
+
+/**
+ * 2. 两数相加
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例 1：
+ *
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
+ *
+ * 示例 2：
+ *
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ *
+ * 示例 3：
+ *
+ * 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * 输出：[8,9,9,9,0,0,0,1]
+ *
+ * 提示：
+ * 每个链表中的节点数在范围 [1, 100] 内
+ * 0 <= Node.val <= 9
+ * 题目数据保证列表表示的数字不含前导零
+ */
+public class AddTwoNumber {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode listNode = addTwoNumbers(l1, l2);
+        System.out.println(listNode);
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = null, head = null;
+        int ca = 0;
+        while (l1 != null || l2 != null) {
+            ListNode curr;
+            int add, val1 = 0, val2 = 0;
+            if (l1 != null) {
+                val1 = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                val2 = l2.val;
+                l2 = l2.next;
+            }
+            add = val1 + val2 + ca;
+            ca = add/10;
+            add = add%10;
+            curr = new ListNode(add);
+            if (result == null) {
+                result = head = curr;
+            } else {
+                head.next = curr;
+                head = head.next;
+            }
+        }
+        if (ca > 0) {
+            head.next = new ListNode(ca);
+        }
+        return result;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
